454479bad3a7dae04f31626633943a8a7e570203
Alexandrov <alex.44747.alex@gmail.com>
2023-12-27 07:22:19 +0300

    q

---
+++ src/test/java/sky/pro/demo_shop/DemoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.demo_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
